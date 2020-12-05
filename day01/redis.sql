632d4522b81da1f04805bafc6972990a9f7da8e7
demoone <denghang19950224@163.com>
2020-12-05 17:45:45 +0800

    内容补充

--- day01/redis.sql
+++ day01/redis.sql
@@ -1,3 +1,12 @@
 #今日学习redis数据库操作
 redis是非关系数据库,呈现的形式为key-value形式
 主要数据格式有五种string(字符串)、hash(哈希)、list(列表)、set(集合)、zset(有序集合)。
+string
+	set key value				创建键值对数据
+	get key					获取值
+	mset key1 value1 key2 value2 ...	创建多个键值对数据
+	mget key1 key2				获取多个值
+	setex key score value		 	设置一个有时间限制的键值对(时间到了会自动消除)
+hash
+	hset key field value 			创建哈希数据
+	hget key field 				或缺一个值
